ab7b40bc84f60a592c2dd28fc427a338c15cfafa
firebearrex <rex.cheng@qq.com>
2021-02-09 18:17:40 +0800

    Updated starting codes for user-admin.js

---
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -0,0 +1,39 @@
+// alert("Welcome to JavaScript!")
+// console.log('Welcome to wbdv course!')
+
+var $tableRows
+var $userRowTemplate
+var $tbody
+
+var $createBtn
+var $editBtn
+var $removeBtn
+
+var $usernameFld
+var $passwordFld
+var $firstNameFld
+var $lastNameFld
+var $roleFld
+var $editFld
+
+var userService = new AdminUserServiceClient();
+
+$(main);
+
+function createUser() {
+
+}
+
+function deleteUser() { … }
+
+function selectUser() { … }
+
+function updateUser() { … }
+
+function renderUsers(users) { … }
+
+function findAllUsers() { … } // optional - might not need this
+
+function findUserById() { … } // optional - might not need this
+
+function main() { … }
