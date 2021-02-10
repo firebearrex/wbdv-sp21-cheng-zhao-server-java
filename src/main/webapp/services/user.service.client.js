98d93cca2d0ba6d2f6f5e085519accf8fd42937e
firebearrex <rex.cheng@qq.com>
2021-02-10 17:16:20 +0800

    updated user-admin.js

--- src/main/webapp/services/user.service.client.js
+++ src/main/webapp/services/user.service.client.js
@@ -5,12 +5,27 @@ function AdminUserServiceClient() {
     this.findUserById = findUserById;
     this.deleteUser = deleteUser;
     this.updateUser = updateUser;
-    this.url = 'https://wbdv-generic-server.herokuapp.com/api/YOURNEUID/users';
+    this.url = 'https://wbdv-generic-server.herokuapp.com/api/001013862/users';
     var self = this;
-    function createUser(user) { … }
-    function findAllUsers() { … }
-    function findUserById(userId) { … }
-    function updateUser(userId, user) { … }
-    function deleteUser(userId) { … }
+
+    function createUser(user) {
+
+    }
+
+    function findAllUsers() {
+
+    }
+
+    function findUserById(userId) {
+
+    }
+
+    function updateUser(userId, user) {
+
+    }
+
+    function deleteUser(userId) {
+
+    }
 
 }
--- src/main/webapp/user-admin/user-admin.controller.client.js
+++ src/main/webapp/user-admin/user-admin.controller.client.js
@@ -1,39 +1,75 @@
 // alert("Welcome to JavaScript!")
 // console.log('Welcome to wbdv course!')
 
-var $tableRows
-var $userRowTemplate
-var $tbody
+(function () {
+    // var $tableRows
+    var $userRowTemplate
+    var $tbody
 
-var $createBtn
-var $editBtn
-var $removeBtn
+    var $createBtn
+    var $editBtn
+    var $updateBtn
+    var $removeBtn
 
-var $usernameFld
-var $passwordFld
-var $firstNameFld
-var $lastNameFld
-var $roleFld
-var $editFld
+    var $usernameFld
+    var $passwordFld
+    var $firstNameFld
+    var $lastNameFld
+    var $roleFld
+    var $editFld
 
-var userService = new AdminUserServiceClient();
+    var userService = new AdminUserServiceClient();
 
-$(main);
+    var users = [
+        {username: "CS4550", password: "02", firstname: 23, lastname: "Spring", role: "student"},
+        {username: "CS2345", password: "03", firstname: 34, lastname: "Spring", role: "faculty"},
+        {username: "CS3456", password: "04", firstname: 45, lastname: "Spring", role: "admin"},
+    ]
 
-function createUser() {
+    function createUser() {
 
-}
+    }
 
-function deleteUser() { … }
+    function deleteUser() {
+    }
 
-function selectUser() { … }
+    function selectUser() {
+    }
 
-function updateUser() { … }
+    function updateUser() {
+    }
 
-function renderUsers(users) { … }
+    function renderUsers(users) {
+    }
 
-function findAllUsers() { … } // optional - might not need this
+    function findAllUsers() {
+    } // optional - might not need this
 
-function findUserById() { … } // optional - might not need this
+    function findUserById() {
+    } // optional - might not need this
 
-function main() { … }
+    function main() {
+        $tbody = $(".wbdv-tbody")
+        $userRowTemplate = $(".wbdv-template")
+
+        $createBtn = $(".wbdv-create")
+        $updateBtn = $(".wbdv-update")
+        $editBtn = $(".wbdv-edit")
+        $removeBtn = $(".wbdv-remove")
+
+        $usernameFld = $("usernameFld")
+        $passwordFld = $("passwordFld")
+        $firstNameFld = $("firstNameFld")
+        $lastNameFld = $("lastNameFld")
+        $roleFld = $("roleFld")
+        $editFld = $("editFld")
+
+        // $(".wbdv-hidden").hide()
+
+
+    }
+
+    $(main);
+
+})();
+
